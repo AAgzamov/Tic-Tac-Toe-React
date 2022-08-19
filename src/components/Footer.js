f3c45f877155122b4e5f87ec00b88265e204df88
AAgzamov <amirkhon.agzamov@gmail.com>
2022-08-19 21:59:41 +0500

    minor fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     let {lightMode} = React.useContext(ThemeContext)
     return (
         <div className={lightMode ? "footer" : "footer__dark-mode"}>
-            <div><a id={lightMode ? "color-fill" : "color-fill__dark-mode"} href="https://github.com/AAzagmov/Tic-Tac-Toe-React" data="GitHub">GitHub</a></div>
+            <div><a id={lightMode ? "color-fill" : "color-fill__dark-mode"} href="https://github.com/AAgzamov/Tic-Tac-Toe-React" data="GitHub">GitHub</a></div>
             <div><a id={lightMode ? "color-fill" : "color-fill__dark-mode"} href="https://www.youtube.com/channel/UCObN423Uc3ZlefU5WxhTaUA"
                     data="YouTube">YouTube</a></div>
             <div className="footer-email">
